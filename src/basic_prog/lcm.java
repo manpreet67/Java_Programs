ca8ace4a7f7bdfa72d04ad4e6eab03b334fda0d7
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-11 22:09:06 +0530

    lcm.java

--- src/basic_prog/lcm.java
+++ src/basic_prog/lcm.java
@@ -1,10 +1,22 @@
 package basic_prog;
-
+import java.util.Scanner;
 public class lcm {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter two numbers: ");
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if(x<y){
+			if(y%x==0)
+				System.out.println("lcm: "+x);
+			else
+				System.out.println("lcm: "+x*y);
+		}
+		else{
+			if(y%x==0)
+				System.out.println("lcm: "+y);
+			else
+				System.out.println("lcm: "+x*y);	
+		}
 	}
-
 }
